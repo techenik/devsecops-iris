d657533e521b47361b48e55915f46d3a09b49b27
Nikhil Gopala <gopala.nikhil@gmail.com>
2021-09-28 11:39:19 -0400

    Task Changes

--- test_app.py
+++ test_app.py
@@ -24,3 +24,19 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+# test to check if feedback loop is working
+def test_feedback_loop():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 3,
+        "sepal_width": 5,
+        "petal_length": 3.2,
+        "petal_width": 4.4,
+        "flower_class": "Iris Virginica"
+    }
+    with TestClient(app) as client:
+        response = client.post("/feedback_loop", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        
